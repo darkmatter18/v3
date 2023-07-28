f28c90458e9a546bda169d399053ba34985a588c
Arkadip Bhattacharya <arkadip.bhattacharya@digite.com>
2023-07-28 12:10:24 +0530

    feat: added colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'deep-blue': '#112240',
+        'neon-violet': '#E15FED',
+        'neon-pink': '#FA58B6',
+        'neon-teal': '#6EDCD9',
+      }
     },
   },
   plugins: [],
